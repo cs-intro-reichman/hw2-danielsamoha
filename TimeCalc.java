a571856471629da1dbb16dc9aa0e45e1578b3501
Daniel Samoha <danielaharon.samoha@post.runi.ac.il>
2025-11-11 17:38:16 +0200

    update

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,14 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+
+        int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+        int minutestoadd = Integer.parseInt(args[1]);
+        minutestoadd += hours * 60 + minutes;
+        int newhours = minutestoadd / 60;
+        int newminutes = minutestoadd - newhours * 60;
+        System.out.println(newhours + ":" + newminutes);
+
+
     }
 }
