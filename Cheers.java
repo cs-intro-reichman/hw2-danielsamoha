2f1b7c9f4f295e7844a8fbc1c2602b7055f00984
Daniel Samoha <danielaharon.samoha@post.runi.ac.il>
2025-11-11 18:24:33 +0200

    update

--- Cheers.java
+++ Cheers.java
@@ -8,6 +8,19 @@ Great work boaz!
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+
+            for (int i = 0;i < args[0].length(); i++){
+                char n = args[0].charAt(i);
+                if(n == 'A' ||n == 'E' ||n == 'I' ||n == 'O' || n== 'U'){
+                        System.out.println("Give me an " + args[0].charAt(i) + ": " + args[0].charAt(i) + "!");
+                }
+                else{
+                        System.out.println("Give me a  " + args[0].charAt(i) + ": " + args[0].charAt(i) + "!");
+                }
+            }
+            System.out.println("What does that spell?");
+            for(int i = 0; i < Integer.parseInt(args[1]); i++){
+                System.out.println(args[0] + "!!!");
+            }
         }
 }
