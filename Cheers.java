d9de5b510be3229911b0c8f11e7ef1468101e855
Daniel Samoha <danielaharon.samoha@post.runi.ac.il>
2025-11-12 14:56:05 +0200

    update

--- Cheers.java
+++ Cheers.java
@@ -12,10 +12,10 @@ public class Cheers {
             for (int i = 0;i < args[0].length(); i++){
                 char n = m.charAt(i);
                 if(n == 'A' ||n == 'E' ||n == 'I' ||n == 'O' || n== 'U'){
-                        System.out.println("Give me an  " + m.charAt(i) + ": " + m.charAt(i) + "!");
+                        System.out.println("Give me an " + m.charAt(i) + ": " + m.charAt(i) + "!");
                 }
-                else{
-                        System.out.println("Give me a  " + m.charAt(i) + ": " + m.charAt(i) + "!");
+                if(n == 'F' ||n == 'H'||n == 'L'||n == 'M'||n == 'N'||n == 'R'||n == 'S'||n == 'X'){
+                        System.out.println("Give me a " + m.charAt(i) + ": " + m.charAt(i) + "!");
                 }
             }
             System.out.println("What does that spell?");
