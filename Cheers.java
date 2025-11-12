9177a81eda1836b60f6e366ac5c7cae0f29afa79
Daniel Samoha <danielaharon.samoha@post.runi.ac.il>
2025-11-12 14:31:58 +0200

    update

--- Cheers.java
+++ Cheers.java
@@ -8,19 +8,19 @@ Great work boaz!
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-
+            String m = args[0].toUpperCase();
             for (int i = 0;i < args[0].length(); i++){
-                char n = args[0].charAt(i);
+                char n = m.charAt(i);
                 if(n == 'A' ||n == 'E' ||n == 'I' ||n == 'O' || n== 'U'){
-                        System.out.println("Give me an " + args[0].charAt(i) + ": " + args[0].charAt(i) + "!");
+                        System.out.println("Give me an " + m.charAt(i) + ": " + m.charAt(i) + "!");
                 }
                 else{
-                        System.out.println("Give me a  " + args[0].charAt(i) + ": " + args[0].charAt(i) + "!");
+                        System.out.println("Give me a  " + m.charAt(i) + ": " + m.charAt(i) + "!");
                 }
             }
             System.out.println("What does that spell?");
             for(int i = 0; i < Integer.parseInt(args[1]); i++){
-                System.out.println(args[0] + "!!!");
+                System.out.println(m + "!!!");
             }
         }
 }
